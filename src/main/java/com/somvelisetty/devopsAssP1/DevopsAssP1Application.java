2b51b48c746c1062f1ca45e3453d440dc13a7a43
SomVelisetty <somvelisetty@gmail.com>
2023-05-21 14:06:04 +0530

    initilize

---
+++ src/main/java/com/somvelisetty/devopsAssP1/DevopsAssP1Application.java
@@ -0,0 +1,13 @@
+package com.somvelisetty.devopsAssP1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsAssP1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevopsAssP1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/somvelisetty/devopsAssP1/DevopsAssP1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.somvelisetty.devopsAssP1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsAssP1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
