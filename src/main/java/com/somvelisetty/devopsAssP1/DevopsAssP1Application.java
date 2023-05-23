9ef644051552c09cd4ac058e04a79010e9da1b22
SomVelisetty <somvelisetty@gmail.com>
2023-05-24 01:13:57 +0530

    bug1

--- src/main/java/com/somvelisetty/devopsAssP1/DevopsAssP1Application.java
+++ src/main/java/com/somvelisetty/devopsAssP1/DevopsAssP1Application.java
@@ -15,6 +15,13 @@ public class DevopsAssP1Application {
 
 	boolean T = false;
 	void test_divide() {
+
+		String speech = "Now is the time for all good people to come to the aid of their country.";
+
+		String substr1 = speech.substring(-1, speech.length());  // Noncompliant; start and end values both bad
+		String substr2 = speech.substring(speech.length(), 0); // Noncompliant, start value must be < end value
+		char ch = speech.charAt(speech.length());  // Noncompliant
+
 		int z = 0;
 		if (T) {
 			// ..
